4d03b0bf79279c160805b67aab2ccf1c69689e52
Claudio César Cabrera Young <62724940+claudiocabrerayoung@users.noreply.github.com>
2022-12-06 07:36:03 -0500

    Creation of required functions
    I've just written functions associated to each class. Specifically, the groups-related functions establish the specific interactions between each role's transactions and their constraints. Also, the subsessions-related function is the one that will enable for the randomization of the allocations to groups and roles.

--- evaluationProject/myTrustGame/__init__.py
+++ evaluationProject/myTrustGame/__init__.py
@@ -36,6 +36,28 @@ class Player(BasePlayer):
     name = models.StringField(label = "Your name:")
     age = models.IntegerField(label = "Your age:")
 
+
+# FUNCTIONS
+
+#On Subsessions
+def creating_session(subsession: Subsession):
+    # .group_randomly will enable the randomization
+    #of the player's allocation to groups, as well as
+    #the role's allocation to players.
+    subsession.group_randomly(fixed_id_in_group=False)
+
+#On Groups
+def sent_back_amount_max(group: Group):
+    return group.sent_amount + C.MULTIPLIER
+
+def set_payoffs(group:Group):
+    p1 = group.get_player_by_role("Trustor")
+    p2 = group.get_player_by_role("Trustee")
+    p1.payoff = C.ENDOWMENT = group.sent_amount + group.sent_back_amount
+    p2.payoff = group.sent_amount * C.MULTIPLIER - group.sent_back_amount
+
+# On Players
+
 # PAGES
 class MyPage(Page):
     pass
